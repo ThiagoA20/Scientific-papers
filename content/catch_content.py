a24cba978ae43e9a574d8f8d0b1e4d911477e465
ThiagoA20 <67110486+ThiagoA20@users.noreply.github.com>
2020-10-17 20:48:27 -0500

    Add files via upload

--- content/catch_content.py
+++ content/catch_content.py
@@ -1,12 +1,11 @@
 import time
 import os
-import requests
+import sys
 import pandas as pd
-import json
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-cwd = os.getcwd() + '/content'
+cwd = os.getcwd() + '/content/originals'
 
 profile = webdriver.FirefoxProfile()
 profile.set_preference("browser.download.folderList", 2)
@@ -22,46 +21,84 @@ profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pd
 file_names = []
 
 def Catch_content(content):
-    url = f"https://pubmed.ncbi.nlm.nih.gov/?term={content.searchTerm}"
-    print(url)
-    i = 0
-    o = 0
-
+    print("\n---Choose your database---")
+    databases = {1:'PubMed',2:'PubChem',3:'Wikipedia Content',4:'SciElo',5:'Core',6:'arXiv',7:'ERIC'}
+    print("\n[1] - PubMed\n[2] - PubChem\n[3] - Wikipedia Content\n[4] - SciElo\n[5] - Core\n[6] - arXiv\n[7] - ERIC\n")
+    database = int(input("Database Num: "))
+    if database != 3:
+        art_amount = int(input("Articles Amount: "))
+    else:
+        art_amount = 1
+    print(f"\nCatching content from: {databases[database]}, Searches Amount: {art_amount}")
     option = Options()
     option.headless = True
-    driver = webdriver.Firefox(firefox_binary="/usr/bin/firefox-esr", firefox_profile=profile)
+    driver = webdriver.Firefox(firefox_binary="/usr/bin/firefox-esr", firefox_profile=profile, options=option)
 
-    while i < 10:
-        driver.get(url)
-        time.sleep(1)
-        if o > 10:
-            driver.find_element_by_xpath("//button[@data-ga-action='show_more']").click()
-            time.sleep(2)
-
-        o += 1
-        try:
-            driver.find_element_by_xpath(f"//a[@data-ga-action='{o}']").click()
-            driver.get(f"https://sci-hub.st/{driver.current_url}")
-            org = driver.find_element_by_xpath("//a[@href='#']")
-            text = org.get_attribute("onclick")
-            ind = []
-            for index, item in enumerate(text):
-                if item == '/':
-                    ind.append(index)
-                elif item == '?':
-                    a = index
-            ind = max(ind) + 1
-            file_names.append(text[ind:a])
-            print(f"Article founds: {i+1}")
-            i += 1
-        except:
-            print('Article not found')
-            continue
-
-    for article in file_names:
-        print(f"{cwd}/{article}.part")
-        while os.path.exists(f"{cwd}/{article}.part"):
+    if database == 1:
+        url = f"https://pubmed.ncbi.nlm.nih.gov/?term={content.searchTerm}"
+        print(url)
+        i = 0
+        o = 0 
+        while i < art_amount:
+            driver.get(url)
             time.sleep(1)
-    
-    driver.quit()
-    return file_names
+            if o > 10:
+                driver.find_element_by_xpath("//button[@data-ga-action='show_more']").click()
+                time.sleep(2)
+
+            o += 1
+            try:
+                driver.find_element_by_xpath(f"//a[@data-ga-action='{o}']").click()
+                driver.get(f"https://sci-hub.st/{driver.current_url}")
+                org = driver.find_element_by_xpath("//a[@href='#']")
+                text = org.get_attribute("onclick")
+                ind = []
+                for index, item in enumerate(text):
+                    if item == '/':
+                        ind.append(index)
+                    elif item == '?':
+                        a = index
+                ind = max(ind) + 1
+                file_names.append(f"{cwd}/{text[ind:a]}")
+                print(f"Article founds: {i+1}")
+                i += 1
+            except:
+                print('Article not found in Sci-hub database')
+                continue
+
+        for article in file_names:
+            time.sleep(1)
+            while os.path.exists(f"{article}.part"):
+                print("Waiting the files download...")
+                time.sleep(2)
+        
+        driver.quit()
+        return file_names
+
+    elif database == 2:
+        driver.quit()
+        sys.exit('Working on PubChem... Select another database')
+
+    elif database == 3:
+        driver.quit()
+        sys.exit('Working on Wikipedia Content... Select another database')
+
+    elif database == 4:
+        driver.quit()
+        sys.exit('Working on SciElo...Select another database')
+
+    elif database == 5:
+        driver.quit()
+        sys.exit('Working on Core... Select another database')
+
+    elif database == 6:
+        driver.quit()
+        sys.exit('Working on arXiv...Select another database')
+
+    elif database == 7:
+        driver.quit()
+        sys.exit('Working on ERIC...Select another database')
+
+    else:
+        driver.quit()
+        sys.exit('Invalid database!')
--- robots/text_robot.py
+++ robots/text_robot.py
@@ -1,3 +1,28 @@
-def txt_rob(content, pdf_path):
-    print(content.searchTerm)
-    print(pdf_path)
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from os.path import abspath
+
+def txt_rob(content, pdf_paths):
+    content.originalContent = ''
+
+    def convert_pdf_to_string(pdf):
+        pdf = PdfFileReader(pdf, strict=False)
+        for page_num in range(pdf.numPages):
+            pageObj = pdf.getPage(page_num)
+
+            try:
+                txt = pageObj.extractText()
+                try:
+                    dirt = open("dirt_content.txt", "r")
+                except:
+                    dirt = open("dirt_content.txt", "w")
+                dirt.writelines(txt)
+                dirt.close()
+            except:
+                pass
+            else:
+                content.originalContent += f"{txt}\n"
+
+    for pdf in pdf_paths:
+        convert_pdf_to_string(abspath(pdf))
+
+    print(content.originalContent)
